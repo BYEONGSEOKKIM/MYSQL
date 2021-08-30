8b2bd17b26de6d4bc4995727e2143eb92abc7565
BYEONGSEOKKIM <87461861+BYEONGSEOKKIM@users.noreply.github.com>
2021-08-30 23:18:58 +0900

    Add files via upload

---
+++ select1.sql
@@ -0,0 +1,11 @@
+use employees;
+select * from employees;
+select * from employees.titles;
+
+select first_name, last_name, gender from employees;
+show databases;
+show tables;
+show table status;
+describe employees;
+desc titles;
+select * from employees where first_name = 'Georgi' and last_name = 'Ariola';
---
+++ select2.sql
@@ -0,0 +1,7 @@
+show databases;
+use employees;
+show table status;
+show tables;
+describe employees;
+select first_name, gender from employees;
+select first_name as 이름, gender 성별, hire_date '회사 입사일' from employees;
